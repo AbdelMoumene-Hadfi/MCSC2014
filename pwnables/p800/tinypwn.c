360293bcd5adbcbb86309c20824cda96c2fe1696
Mohamed Ghannam <simo.ghannam@gmail.com>
2014-06-10 13:47:22 +0100

    Update tinypwn.c

--- pwnables/p800/tinypwn.c
+++ pwnables/p800/tinypwn.c
@@ -1,8 +1,8 @@
 /* Advanced ROP Exploitation 
  * By : Mohamed Ghannam
  * no gadgets anymore :-) ,let's know if you got it 
- * HINT : RAX registers returns something interesting after 
- *	write() being called , process limit might help
+ * HINT : EAX registers returns something interesting after 
+ *	read() being called
  */
 
 void _start() {
